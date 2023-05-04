ba74d722122838c9a1f11667295b19561430e047
mkhuleko sehlabela <m.sihlabela@lab.clicklearning.org>
2023-05-04 20:15:10 +0200

    bb

--- IWA02/Javascript.js
+++ IWA02/Javascript.js
@@ -6,7 +6,7 @@ const user = 'John'
 /* This is the lastname of the user */ const surname = 'Smith'
 
 
-console.log(user; surname;)
+console.log(user, surname)
 
 /* (c) ACME Inc. 2010 */
 
@@ -28,5 +28,5 @@ console.warn(Security scan starting)
 
 /* It is important to let user know when they can close the page  */
 
-cnosole.log(info)('Please wait for scan to complete before closing the browser.)
+console.info('Please wait for scan to complete before closing the browser')
  
