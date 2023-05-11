053e582c407f41619162996f44330b15446cc2af
mkhuleko sehlabela <m.sihlabela@lab.clicklearning.org>
2023-05-11 11:02:02 +0200

    kk

--- IWA04/java.js
+++ IWA04/java.js
@@ -3,36 +3,35 @@ const status= 'student'
 const count = 0
 
 
-if (date = 2050) 
-	console.log('January, New Year’s Day')
-	console.log("March", 'Human Rights Day')
-	date = 'April'
-	console.log(date, 'Family Day')
-	console.log(date, 'Freedom Day')
-	let.count = count + 4 
+if (date = 2050) {
+	console.log(date, "January", 'New Year’s Day')
+	console.log(date, "March", 'Human Rights Day')
+	
+	console.log(date, 'April','Family Day')
+	console.log(date,'April','Freedom Day')
+	var count = count + 4 
 
 
 	if (status = "student") {
-	  console.log('June', 'Youth Day')
-		let.count = count + 1
+	  console.log(date,'June','Youth Day')
+		var count = count + 1
   }
 
-	console.log('August', 'Women’s Day')
-	console.log('September', 'Heritage Day')
-	date = 'December'
+	console.log(date,'August', 'Women’s Day')
+	console.log(date,'September','Heritage Day')
 	console.log(date,'Day of Reconciliation')
-	let.count = count + 3
+	var count = count + 3
 
 
-	if (status = 'parent') 
-	  console.log(date,'Christmas Day')
-		let.count = count + 1
+	if (status = 'parent') {
+	  console.log(date,'December','Christmas Day')
+		var count = count + 1
+	}
+	if (status = "student") {
+	console.log(date, 'Day of Goodwill')
+	var count = count + 1}
   
-
-	console.log(date,'Day of Goodwill')
-	let. count = count + 1
-  
-
+	}
 console.log('Your status is:', status)
 console.log('The year is:', date)
 console.log('The total holidays is:', count)
