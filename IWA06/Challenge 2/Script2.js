6c94d0ae6e9cc369728dd74d7bd4dbc646260cbe
mkhuleko sehlabela <m.sihlabela@lab.clicklearning.org>
2023-05-29 11:26:56 +0200

    fixed

--- IWA06/Challenge 2/Script2.js
+++ IWA06/Challenge 2/Script2.js
@@ -11,6 +11,7 @@ const minuteOfDay = '00';
 if (hourOfDay === 0 && minuteOfDay === 0); {
 const taxAsDecimal = (tax/ 100);
 const startingAfterTax = salary * (1 - taxAsDecimal);
-const balace = (starting - transport - food - rent);
+const balace = ("starting" - "transport" - "food" - "rent");
 console.log('R274.05 (balance.toFixed(2))')
 }
+
