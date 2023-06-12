d10417a08bedb42a632bcb0949910dd82ffffeb2
mkhuleko sehlabela <m.sihlabela@lab.clicklearning.org>
2023-06-12 09:01:49 +0200

    ghhg

--- IWA13/Script.js
+++ IWA13/Script.js
@@ -1,24 +1,24 @@
 let state = 'idle'
 let user = null
-let calculated = '1'
+let calculated = 1;
 
 // Only allowed to change below
 
 
-const = logCalc () => { 
-    const isString = typeof calculated = 'numerical-string' 
-    const calculatedAsNumber = isString ? calculated : parseNumber(calculated)
+const logCalc = () => { 
+    const isString = typeof calculated === 'string' 
+    const calculatedAsNumber = isString ? parseInt(calculated) : calculated;
     calculated === calculatedAsNumber + 1 
 }
 
-const = calcUser () => {
+const calcUser = () => {
   logCalc
   if (calculated > 2) user = 'John'
   if (calculated > 2) state = 'requesting'
   if (calculated > 3) state = 'idle'
 }
 
-const = checkUser () => {
+const checkUser = () => {
 	if (user && state === 'requesting') {
 		console.log(`User: ${user} (${calculated})`)
 	}
